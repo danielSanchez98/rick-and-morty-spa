0f20b9e2c366bdacae8ab89895dfda51402abf9a
Daniel <ds980220@gmail.com>
2020-12-31 22:07:23 -0500

    fix redirect to home

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/rick-and-morty-spa/#">
+                    <a href="#/rick-and-morty-spa/#">
                         100tifi.co
                     </a>
                 </h1>
